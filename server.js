b68cb2445085c7a0d3e6543cc35cf53207bd75f0
Will Owen <owen.will.m@gmail.com>
2025-06-08 09:24:25 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+// Basic HTTP server using Node.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from your first backend server!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
