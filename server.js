8023ca779a9cb1705cc2b5f0366a2b2da9085044
owen-will-m <wallowin66@gmail.com>
2025-06-08 19:18:42 -0700

    update

--- server.js
+++ server.js
@@ -11,10 +11,25 @@ app.get("/bus", async (req, res) => {
       "https://webservices.umoiq.com/api/pub/v1/agencies/sfmta-cis/stopcodes/13911/predictions?key=0be8ebd0284ce712a63f29dcaf7798c4"
     );
     const data = await response.json();
-    res.json(data); // send it back to client
+    let times = data.map((item) => ({}));
+
+    const result = data.flatMap((item) => {
+      if (!item.values || !Array.isArray(item.values)) return [];
+
+      return item.values.map((prediction) => ({
+        route: item.route?.title ?? "Unknown route",
+        destination:
+          prediction.direction?.destinationName ?? "Unknown destination",
+        minutesAway: prediction.timestamp
+          ? Math.round((prediction.timestamp - Date.now()) / 60000)
+          : null,
+      }));
+    });
+
+    res.json(result); // send it back to client
   } catch (err) {
     console.error("API error:", err);
-    res.status(500).send("Error fetching predictions");
+    res.status(500).send(`Error fetching predictions ${err}`);
   }
 });
 
